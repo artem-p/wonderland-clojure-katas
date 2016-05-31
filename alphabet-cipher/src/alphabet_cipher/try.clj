a6d8e533fdb49d15967857c58449185281b2677d
Artem Pugachev <artempugachev1@gmail.com>
2016-05-31 23:16:52 +0300

    circle shift

--- alphabet-cipher/src/alphabet_cipher/try.clj
+++ alphabet-cipher/src/alphabet_cipher/try.clj
@@ -3,7 +3,8 @@
 (defn circle-shift
   "Сдвигаем первый символ строки в конец"
   [s]
-  (apply str(concat (rest s) [(first s)]))
-  )
+  (apply str(concat (rest s) [(first s)])))
 
 (circle-shift alphabet)
+
+; todo тест для circle-shift
