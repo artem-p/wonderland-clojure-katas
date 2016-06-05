855152482d9143712f121539b20fb818e53d0834
Artem Pugachev <artempugachev1@gmail.com>
2016-06-05 22:49:46 +0300

    Получаем букву ключевого слова

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -33,17 +33,34 @@
 (def key-word "scones" )
 
 ;; m e e t m e b y t h   e   t   r   e   e
-;; 1 2 3 4 5 6 7 8 9 10  11  12  13  14  15
-;; 1 2 3 4 5 6 1 2 3 4   5   6   1   2   3
+;; 0 1 2 3 4 5 6 7 8 9   10  11  12  13  14
+;; 0 1 2 3 4 5 0 1 2 3   4   5   0   1   2   3
 
 (defn get-pos-in-keyword
   "Для индекса символа в сообщении получаем индекс в ключевом слове"
   [index-in-message keyword-len]
-  ;; Если номер меньше или равен длине ключевого слова, возвращаем его. Если больше, вызываем еще раз, вычитая из номера длину ключевого слова
-  (if (<= index-in-message keyword-len) index-in-message (get-pos-in-keyword (- index-in-message keyword-len) keyword-len)))
+  ;; Если номер меньше длины ключевого слова, возвращаем его. Если больше, вызываем еще раз, вычитая из номера длину ключевого слова
+  (if (< index-in-message keyword-len) index-in-message (get-pos-in-keyword (- index-in-message keyword-len) keyword-len)))
 
-(= (get-pos-in-keyword 1 6) 1)
-(= (get-pos-in-keyword 2 6) 2)
-(= (get-pos-in-keyword 6 6) 6)
-(= (get-pos-in-keyword 7 6) 1)
+
+(defn get-keyword-letter
+  "Получаем букву ключевого слова по индексу символа сообщения"
+  [index-in-message key-word]
+  (get key-word (get-pos-in-keyword index-in-message (count key-word)))
+  )
+
+(= (get-keyword-letter 1 "abc") \b)
+(= (get-keyword-letter 2 "abc") \c)
+(= (get-keyword-letter 3 "abc") \a)
+
+(= (get-keyword-letter 0 "scones") \s)
+(= (get-keyword-letter 7 "scones") \c)
+
+
+;; (= (get-pos-in-keyword 1 6) 1)
+;; (= (get-pos-in-keyword 2 6) 2)
+;; (= (get-pos-in-keyword 6 6) 6)
+;; (= (get-pos-in-keyword 7 6) 1)
+;; (= (get-pos-in-keyword 9 6) 3)
+;; (= (get-pos-in-keyword 14 6) 2)
 ; todo еще проверки
